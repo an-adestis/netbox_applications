efac855c2ba091967a14c21a19504f0c13d95924
amin nabiyev <a.nabiyev@adestis.de>
2025-05-15 11:40:26 +0200

    Readme.md

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='adestis-netbox-applications',
-    version='1.0.3',
+    version='1.0.4',
     description='ADESTIS Application Management',
     # url='https://github.com/adestis/netbox-account-management',
     author='ADESTIS GmbH',
