9975667b8d6c55f0c2dcac20972d6545de661420
amin nabiyev <a.nabiyev@adestis.de>
2025-08-06 13:47:17 +0200

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='adestis-netbox-applications',
-    version='1.0.13',
+    version='1.0.14',
     description='ADESTIS Application Management',
     url='https://github.com/an-adestis/netbox_applications',
     author='ADESTIS GmbH',
