8b802ae11d1344e31d2116a1d043ccb506a4e749
amin nabiyev <a.nabiyev@adestis.de>
2026-04-08 10:14:15 +0200

    version erhöhen

--- adestis_netbox_applications/__init__.py
+++ adestis_netbox_applications/__init__.py
@@ -4,7 +4,7 @@ class AdestisApplicationsConfig(PluginConfig):
     name = 'adestis_netbox_applications'
     verbose_name = 'Application Management'
     description = 'A NetBox plugin for managing applications.'
-    version = '1.0.23'
+    version = '1.0.25'
     author = 'ADESTIS GmbH'
     author_email = 'pypi@adestis.de'
     base_url = 'applications'
